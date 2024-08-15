8a07f2476ba252add1a9163f7c3484365508ce91
Luke Christopulos <lukechristopulos@gmail.com>
2024-08-15 09:29:32 -0500

    Initial Commit

---
+++ All About Me/All_About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_About_MeApp.swift
+//  All About Me
+//
+//  Created by Luke R. Christopulos on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct All_About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All About Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  All About Me
+//
+//  Created by Luke R. Christopulos on 8/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
